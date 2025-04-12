1e399f0b5dba28a45e45fde77ed4d887bf517a34
Endrew_Carlos <endrewcarlos2006@gmail.com>
2025-04-12 05:30:38 +0000

    Batalha Naval Desafio: nível Mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,7 +6,32 @@
 int main() {
     char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
     int tabuleiro[LINHAS][COLUNAS];
+    
+    //Matriz da cruz
+    int cruz[5][5] = {
+        {0, 0, 5, 0, 0},
+        {0, 5, 5, 5, 5},
+        {0, 0, 5, 0, 0}
+    };
+    
+    // Parte horizontal da cruz
+    for (int j = 0; j < 5; j++) {
+        cruz[1][j] = 5;
+    
+    //Matriz do cone
+    int cone[3][5] = {
+        {0, 0, 5, 0, 0},
+        {0, 5, 5, 5, 0},
+        {5, 5, 5, 5, 5}
+    };
 
+    //Matriz do octaedro
+    int octaedro[3][3] = {
+        {0, 5, 0},
+        {5, 5, 5},
+        {0, 5, 0}
+    };
+    
     //Representação da água e o posicionamento dos navios nas diagonais
     for (int i = 0; i < LINHAS; i++) {
         for (int j = 0; j < COLUNAS; j++){
@@ -14,11 +39,11 @@ int main() {
             if (LINHAS == COLUNAS)
             {
                 tabuleiro[i][i - 0] = 3; //diagonal principal
-                tabuleiro[i][10 - 1 - i] = 3; //diagonal segundária
+                //tabuleiro[i][10 - 1 - i] = 3; //diagonal segundária
             }
         }
     }
-
+    
     //Posicionamento dos navios na vertical e horizontal
     tabuleiro[2][3] = 3; 
     tabuleiro[2][4] = 3;
@@ -27,6 +52,33 @@ int main() {
     tabuleiro[6][7] = 3;
     tabuleiro[7][7] = 3;
 
+    //Habilidade da cruz 
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (cruz[i][j] != 0) {
+                tabuleiro[i][j + 5] = cruz[i][j];
+            }
+        }
+    }
+
+    //Habilidade do cone 
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (cone[i][j] != 0) {
+                tabuleiro[i + 7][j + 2] = cone[i][j];
+            }
+        }
+    }
+
+    //Habilidade do octaedro
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (octaedro[i][j] != 0) {
+                tabuleiro[i + 2][j + 0] = octaedro[i][j];
+            }
+        }
+    }
+    
     //Número de Colunas
     printf(" TABULEIRO BATALHA NAVAL\n");
     printf("   ");
@@ -46,4 +98,5 @@ int main() {
     
     
     return 0;
+}
 }
